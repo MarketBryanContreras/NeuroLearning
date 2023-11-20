7a691476ba2b9c13f6a6747a91138d4eb088aa9b
Bryan Contreras <bcont999@outlook.com>
2023-11-20 12:02:51 -0500

    Started working on sleep analysis pipeline in Sandbox

---
+++ Sandboxes/BC_sandbox_sleep_NOPR_analys.m
@@ -0,0 +1,8 @@
+%% Sandbox for NOPR analsys
+
+%% Initialize
+% Go to the directory of some data
+data_dir= '/Users/bryancontrerasmercado/Williams Lab Dropbox/Williams Lab Team Folder/Bryan_DropBox/CHRNA2_NOVEL_OBJECT/raw_data/NOPR/BC011_2023_10_10_D2_NOPR';
+cd(data_dir)
+
+%Load the required channels
